11044496cb10f8b957310b28bf031f171f77465c
SonBanQuyenn <sonnt.namchau@gmail.com>
2026-05-17 23:52:54 +0700

    Add files via upload

---
+++ ex5.js
@@ -0,0 +1,13 @@
+let numberA = 5;
+let stringB = "5";
+
+console.log("Kết quả so sánh numberA == stringB:", numberA == stringB); // true
+
+console.log("Kết quả so sánh numberA === stringB:", numberA === stringB); // false
+
+let userConfirmed = confirm("Bạn đã hiểu bài chưa?");
+if (userConfirmed) {
+console.log("Người dùng đã hiểu bài.");
+} else {
+console.log("Người dùng chưa hiểu bài.");
+}
